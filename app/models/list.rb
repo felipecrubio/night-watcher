46d14a7d75a811d495f556418ba9f9749643cbc4
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2022-08-18 14:15:45 -0300

    create models Movie, List, Bookmark

---
+++ app/models/list.rb
@@ -0,0 +1,6 @@
+class List < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
+
+  validates :name, presence: true, allow_blank: false, uniqueness: { case_sensitive: false }
+end
