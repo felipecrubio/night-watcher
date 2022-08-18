2189911c9cb225ac509d767ee61af007551f88b4
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2022-08-18 19:27:07 -0300

    created user stories: index, list all, list one. implementing new list

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,13 +16,17 @@ url = 'http://tmdb.lewagon.com/movie/top_rated'
 response = JSON.parse(URI.open(url).read)
 
 response['results'].each do |movie_hash|
-  puts
-  p movie_hash
   Movie.create!(
     title: movie_hash['original_title'],
     overview: movie_hash['overview'],
     poster_url: "https://image.tmdb.org/t/p/w500#{movie_hash['poster_path']}",
     rating: movie_hash['vote_average']
+  )
+end
+
+6.times do
+  List.create!(
+    name: Faker::TvShows::Supernatural.creature
   )
 end
 puts 'Finished!'
